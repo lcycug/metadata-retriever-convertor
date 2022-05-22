dfd2518c9065a3796723d6b6d80113ee067e35a2
Sean Lee <snuffle_earn.0i@icloud.com>
2022-05-22 22:32:06 +0800

    :construction: Powershell

--- sourceRetriever.ps1
+++ sourceRetriever.ps1
@@ -5,44 +5,49 @@ param(
     [String]$username
 )
 # Variables
-$tempFolder = "temp"
-$sourceFolder = "source"
-$sourceDotZip = "source.zip"
+$checkMark="$([char]0x1b)[92m$([char]8730)"
+$manifest = "manifest"
 $outputJson = "output.json"
-$unPackagedFolder = "unpackaged"
-$unPackagedDotZip = "unpackaged.zip"
 $packageDotXml = "package.xml"
 $packageDotXmlFooter = "<version>54.0</version></Package>"
+$packageDotXmlHeader = '<?xml version="1.0" encoding="UTF-8"?><Package xmlns="http://soap.sforce.com/2006/04/metadata">'
+$redX="$([char]0x1b)[91mx"
+$sourceDotZip = "source.zip"
+$sourceFolder = "source"
+$tempFolder = "SourceRetrieverTemporaryFolder"
 $template = "<types><members>*</members><name>line</name></types>"
-$packageDotXmlHeader = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Package xmlns="http://soap.sforce.com/2006/04/metadata">'
+$unPackagedDotZip = "unpackaged.zip"
+$unPackagedFolder = "unpackaged"
 # --- Main ---
-# Listing connections
+Write-Output "$checkMark Listing connections..."
 sfdx force:org:list
-# Testing if in a SFDX project
-mkdir $tempFolder
-Write-Output "$packageDotXmlHeader$packageDotXmlFooter" > "$tempFolder/$packageDotXml"
-sfdx force:mdapi:convert -r $tempFolder -d "$tempFolder/$sourceFolder"
-if (Test-Path "$tempFolder/$sourceFolder" -PathType Any)
+Write-Output "$checkMark Testing if in a SFDX project..."
+Write-Output "$checkMark Removing the temporary folder and files if existing..."
+Remove-Item $tempFolder -Recurse
+Remove-Item $sourceDotZip
+New-Item -Path .\ -Name $tempFolder -ItemType "directory" -Force
+Write-Output "$packageDotXmlHeader$packageDotXmlFooter" > "$tempFolder\$manifest\$packageDotXml"
+sfdx force:mdapi:convert -r "$tempFolder\$manifest" -d "$tempFolder\$sourceFolder"
+if (-Not(Test-Path "$tempFolder/$sourceFolder" -PathType Any))
 {
-    rm -rf $tempFolder
-}
-else
-{
-    rm -rf $tempFolder
+    Write-Output "$redX Failed the testing..."
     exit 1
 }
+Write-Output "$checkMark Changing directory into the temporary one..."
+Set-Location -Path $tempFolder
 if ($username -eq "")
 {
     sfdx force:org:display --json > $outputJson
     $jsonElements = Get-Content $outputJson -Raw | ConvertFrom-Json
     $username = $jsonElements.result.alias
-    Write-Output "Requesting metadata with the default connection: $username"
+    Write-Output "$checkMark Requesting metadata with the default connection: $username"
 }
 else
 {
-    Write-Output "Requesting metadata with the given connection:  $username"
+    Write-Output "$checkMark Requesting metadata with the given connection:  $username"
 }
-# Retrieving metadata description
+
+Write-Output "$checkMark Retrieving metadata description..."
 sfdx force:mdapi:describemetadata -u $username -f $outputJson
 # Writing header into package.xml
 Write-Output $packageDotXmlHeader > $packageDotXml
@@ -52,31 +57,28 @@ foreach ($element in $jsonElements.metadataObjects)
 {
     Write-Output ($template -replace "line", $element.xmlName) >> $packageDotXml
 }
-rm $outputJson
-# Writing package footer into package.xml
+Write-Output "$checkMark Writing package footer into package.xml..."
 Write-Output $packageDotXmlFooter >> $packageDotXml
-# Retrieving metadata
-sfdx force:mdapi:retrieve -u $username -r ./ -k $packageDotXml
-# Removing package.xml
-rm $packageDotXml
+Write-Output "$checkMark Retrieving metadata..."
+sfdx force:mdapi:retrieve -u $username -r .\
 if (Test-Path $unPackagedDotZip -PathType Any)
 {
-    Write-Output "Unpackaged.zip retrieval done."
+    Write-Output "$checkMark $unPackagedDotZip retrieval done."
 }
 else
 {
-    Write-Output "Unpackaged.zip retrieval failed."
+    Write-Output "$checkMark $unPackagedDotZip retrieval failed."
     exit 1
 }
-# Unzipping unpackaged.zip into unpackaged folder
+Write-Output "$checkMark Unzipping $unPackagedDotZip into $unPackagedFolder folder..."
 Expand-Archive -Path $unPackagedDotZip -DestinationPath $unPackagedFolder
-# Converting metadata into source format
+Write-Output "$checkMark Converting metadata into source format..."
 sfdx force:mdapi:convert -r $unPackagedFolder -d $sourceFolder
-rm $unPackagedDotZip
-rm -rf $unPackagedFolder
-# Zippping source files into source.zip
-rm -rf $sourceDotZip
+Write-Output "$checkMark Zipping $sourceFolder files into $sourceDotZip..."
 Compress-Archive -Path $sourceFolder -DestinationPath $sourceDotZip
-rm -rf $sourceFolder
-Write-Output "Metdata retrieval done."
+Copy-Item $sourceDotZip -Destination .\..\
+Set-Location -Path .\..\
+Write-Output "$checkMark Removing the temporary folder..."
+Remove-Item $tempFolder -Recurse
+Write-Output "$checkMark Metdata retrieval done."
 exit 0
